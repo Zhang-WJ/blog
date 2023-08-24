668d5dd7158b9c6583e20c42d259318bd41c27d8
zhangweijun <ahnqihyun@sina.cn>
2023-08-24 14:31:25 +0800

    first commit

---
+++ posts/first-step-fp.md
@@ -0,0 +1,13 @@
+---
+title: Functional Programming
+published_at: 2023-08-24T15:00:00.000Z
+snippet: why need functional programming.
+---
+
+#### 函数式编程带来的是一种思维方式
+- 当你深陷mutable中的时候，immutable可以作为一个思路。 
+- 当你在OO中不断的Encapsulate，FP的partial，currying，composition等等，让问题更加明确。
+- type inference带的来得便利是可以写一些更加通用的code。同时及时发现你的错误。
+- pattern match可以把问题模型更加清楚，（但是当你的模型变得复杂使用When后或者wildcat之后）。
+- option 类型可以让你处理错误的时候变得睿智。
+
---
+++ utils/posts.ts
@@ -0,0 +1,38 @@
+import { extract } from "$std/front_matter/any.ts";
+import { join } from "$std/path/mod.ts";
+
+export interface Post {
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  content: string;
+  snippet: string;
+}
+
+export async function getPost(slug: string): Promise<Post | null> {
+  try {
+    const text = await Deno.readTextFile(join("./posts", `${slug}.md`));
+    const { attrs, body } = extract(text);
+    return {
+      slug,
+      title: attrs.title as string,
+      publishedAt: new Date(attrs.published_at as number),
+      content: body,
+      snippet: attrs.snippet as string,
+    };
+  } catch (_: unknown) {
+    return null;
+  }
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir("./posts");
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
+}
